d41d9c96122914d6ea7ebf34d5c84374624ab0e5
jaden <hyunbin.kim.3376@gmail.com>
2022-03-20 18:44:27 +0900

    logo

--- src/views/Nav.js
+++ src/views/Nav.js
@@ -1,12 +1,33 @@
 import React from "react";
 import styled from "styled-components";
-
+import LogoImg from "./shose.png";
 const Common = styled.div`
   display: flex;
+  justify-content: space-between;
+  margin: 20px;
+`;
+
+const Home = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 30px;
+  margin: 10px;
+  text-decoration: none;
+  color: lightgreen;
+  font-weight: bold;
+`;
+
+const Logo = styled.img`
+  width: 100px;
 `;
 
 const Nav = () => {
-  return <Common></Common>;
+  return (
+    <Common>
+      <Home href="/">ShoeMar</Home>
+      <Logo src={LogoImg} />
+    </Common>
+  );
 };
 
 export default Nav;
