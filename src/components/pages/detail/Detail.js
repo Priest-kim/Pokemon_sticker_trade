32284ce9d7b3fe759ae290a6d0843f06adfcde28
jaden <hyunbin.kim.3376@gmail.com>
2022-04-10 15:58:00 +0900

    detail layout

--- src/components/pages/detail/Detail.js
+++ src/components/pages/detail/Detail.js
@@ -5,7 +5,28 @@ const Detail = () => {
   return (
     <Wrapper>
       <MonsterImageArea></MonsterImageArea>
-      <MonsterInfomationArea></MonsterInfomationArea>
+      <MonsterInfomationArea>
+        <MainTitleBox>
+          <p>영어 이름</p>
+          <p>한글 이름</p>
+        </MainTitleBox>
+        <ButtonArea></ButtonArea>
+        <MonsterDetailArea>
+          <p>타입 : </p>
+          <p>
+            Bulbasaur is a small, quadrupedal amphibian Pokémon that has
+            blue-green skin with darker patches. It has red eyes with white
+            pupils, pointed, ear-like structures on top of its head, and a
+            short, blunt snout with a wide mouth. A pair of small, pointed teeth
+            are visible in the upper jaw when its mouth is open. Each of its
+            thick legs ends with three sharp claws. On Bulbasaur's back is a
+            green plant bulb, which is grown from a seed planted there at birth.
+            The bulb also conceals two slender, tentacle-like vines and provides
+            it with energy through photosynthesis as well as from the
+            nutrient-rich seeds contained within.
+          </p>
+        </MonsterDetailArea>
+      </MonsterInfomationArea>
     </Wrapper>
   );
 };
@@ -48,5 +69,29 @@ const MonsterInfomationArea = styled.div`
 `;
 
 const MainTitleBox = styled.div`
-  width: 100%;
+  padding: 10px 0 0 40px;
+  font-size: 2rem;
+
+  p:first-child {
+    font-size: 3rem;
+    font-weight: bold;
+  }
+`;
+
+const ButtonArea = styled.div`
+  background-color: lightseagreen;
+  height: 200px;
+`;
+
+const MonsterDetailArea = styled.div`
+  padding: 60px 30px;
+  background-color: lightsalmon;
+  height: 100%;
+  text-align: center;
+  p:first-child {
+    font-size: 2rem;
+  }
+  p:last-child {
+    font-size: 1.5em;
+  }
 `;
